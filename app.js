5e8e4ed5ed69be2acade24eda602f27fd695eda8
melanie <mjharv@hotmail.com>
2024-10-31 15:59:07 -0600

    FINALLY-Bosshealth-drops

--- app.js
+++ app.js
@@ -3,8 +3,47 @@
 
 
 //#region 🗃️ Arrays
+const heroes = [
+    {
+        name: 'Oscar',
+        type: 'hotdog',
+        damage: 5,
+        health: 100
+    },
+    {
+        name: 'Meera',
+        type: 'witch',
+        damage: 10,
+        health: 50
+    }
+]
 
+const boss = {
+    health: 100,
+    maxHealth: 100,
+    damage: 5,
+    level: 1
+}
 //#endregion
+
+
+function decreaseBossHealth() {
+    const trioofPumpkins = boss
+    trioofPumpkins.health -= 15
+    console.log('this is health', trioofPumpkins)
+    drawHealth()
+}
+
+
+// function decreaseBossHealth() {
+//     for (let i = 0; i < heroes.length; i++) {
+//         const heroe = heroes[i]
+//         let healthDecrease = 15
+//         console.log('health', healthDecrease)
+//     }
+// }
+
+
 
 
 //#region 🧠 Logic
@@ -15,5 +54,13 @@
 
 
 //#region 🖌️ Drawing
+// ANCHOR the - don't matter in element ID pulling from HTML
+function drawHealth() {
+    const trioofPumpkinsElem = document.getElementById('Trio of Pumpkins')
+    const trioofPumpkinsParagraphElem = trioofPumpkinsElem.querySelector('h2')
+    const trioofPumpkins = boss
+    trioofPumpkinsParagraphElem.innerText = `Trio of Pumpkins | Health ${trioofPumpkins.health}`
+}
+
 //#endregion
 
